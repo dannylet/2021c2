9ef988fc67d3db5d24fcfd1ae3187422cb5a3fb8
unknown <dannylet>
2022-04-08 12:00:55 +0800

    add week07

--- week07/week07-4.cpp
+++ week07/week07-4.cpp
@@ -1,1 +1,15 @@
-
+#include<Stdio.h>
+char line[2000][80];
+int main()
+{
+	int N;
+	scanf("%d\n",&N);
+	for(int i=0;i<N;i++)
+	{
+		gets(line[i]);
+	}
+	for(int i=0;i<N;i++)
+	{
+		printf("%s\n",line[i]);
+	}
+}
